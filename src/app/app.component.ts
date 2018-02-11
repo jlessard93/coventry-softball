61b4db637dde9b115632c201c6a38b87d4efb439
Joshua Lessard <jlessard93@gmail.com>
2018-02-11 15:49:10 -0500

    added add-player option added more frameworks to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,4 @@
 import {Component} from '@angular/core';
-import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
-import {Observable} from "rxjs/Observable";
-
-export class Player {
-  name: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -13,26 +7,10 @@ export class Player {
 })
 export class AppComponent {
 
-  playersCollection: AngularFirestoreCollection<Player>;
-  players: Observable<Player[]>;
-
-  constructor(private db: AngularFirestore) {
-
+  constructor() {
   }
 
   ngOnInit() {
-    this.playersCollection = this.db.collection('players');
-    this.players = this.playersCollection.valueChanges();
   }
 
-  addPlayer(aPlayer: Player) {
-    // Add a new document with a generated id.
-    this.db.collection("players").add({aPlayer})
-      .then(function (docRef) {
-        console.log("Document written with ID: ", docRef.id);
-      })
-      .catch(function (error) {
-        console.error("Error adding document: ", error);
-      });
-  }
 }
